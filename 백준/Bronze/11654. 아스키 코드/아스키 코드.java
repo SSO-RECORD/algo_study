184de92298c9cfdf1bc28effa5c44ae9dd47236f
SSO-RECORD <108278816+SSO-RECORD@users.noreply.github.com>
2024-07-29 21:04:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//next()로 문자열 입력받아 charAt(0)으로 첫번째 문자 꺼내오기
+		int x = sc.next().charAt(0);
+		System.out.println(x);
+		
+
+	}
+
+}
