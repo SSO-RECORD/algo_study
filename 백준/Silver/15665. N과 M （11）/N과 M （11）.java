033615a163fbde2e3ced94a79f978516307e75b1
SSO-RECORD <108278816+SSO-RECORD@users.noreply.github.com>
2024-08-24 22:48:28 +0900

    [Silver II] Title: N과 M (11), Time: 1256 ms, Memory: 337400 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static int N, M;
+	private static int[] input;
+	private static int[] numbers;
+	private static StringBuilder sb;
+	private static Set<String> resultSet;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		resultSet = new HashSet<>();
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		input = new int[N];
+		numbers = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(input);
+		perm(0);
+		System.out.print(sb);
+	}
+
+	private static void perm(int idx) {
+		// 기저조건
+		if (idx == M) {
+			StringBuilder temp = new StringBuilder();
+			for (int num : numbers) {
+				temp.append(num + " ");
+			}
+			String result = temp.toString().trim();
+			if (!resultSet.contains(result)) {
+				resultSet.add(result);
+				sb.append(result + "\n");
+			}
+			return;
+		}
+		// 유도파트
+		for (int i = 0; i < N; i++) {
+			numbers[idx] = input[i];
+			perm(idx + 1);
+		}
+	}
+}
