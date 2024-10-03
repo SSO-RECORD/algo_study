30e0839ef1117c250a9a17bec4565dbfdcbbe851
SSO-RECORD <108278816+SSO-RECORD@users.noreply.github.com>
2024-10-03 16:22:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 108 ms, Memory: 17,948 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -7,11 +7,11 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
+	static boolean[] visited;
 	static List<Integer> mag1;
 	static List<Integer> mag2;
 	static List<Integer> mag3;
 	static List<Integer> mag4;
-	static boolean[] visited;
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/SWEA4013_sample_input.txt"));
@@ -19,126 +19,127 @@ public class Solution {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		int T = Integer.parseInt(br.readLine().trim()); // 테스트케이스 개수
+		int T = Integer.parseInt(br.readLine().trim()); // 총 테스트케이스 수
 		// T만큼 반복
-		for (int tc = 1; tc <= T; tc++) {
+		for (int t = 1; t <= T; t++) {
 			int K = Integer.parseInt(br.readLine().trim()); // 자석을 회전시키는 횟수
 
-			// 1번 자석
-			mag1 = new ArrayList<>();
+			// 1번 자석의 자성정보
 			st = new StringTokenizer(br.readLine());
+			mag1 = new ArrayList<>();
 			for (int i = 0; i < 8; i++) {
 				mag1.add(Integer.parseInt(st.nextToken()));
 			}
 
-			// 2번 자석
-			mag2 = new ArrayList<>();
+			// 2번 자석의 자성정보
 			st = new StringTokenizer(br.readLine());
+			mag2 = new ArrayList<>();
 			for (int i = 0; i < 8; i++) {
 				mag2.add(Integer.parseInt(st.nextToken()));
 			}
 
-			// 3번 자석
-			mag3 = new ArrayList<>();
+			// 3번 자석의 자성정보
 			st = new StringTokenizer(br.readLine());
+			mag3 = new ArrayList<>();
 			for (int i = 0; i < 8; i++) {
 				mag3.add(Integer.parseInt(st.nextToken()));
 			}
 
-			// 4번 자석
-			mag4 = new ArrayList<>();
+			// 4번 자석의 자성정보
 			st = new StringTokenizer(br.readLine());
+			mag4 = new ArrayList<>();
 			for (int i = 0; i < 8; i++) {
 				mag4.add(Integer.parseInt(st.nextToken()));
 			}
 
-			// 자석의 회전 정보
-			int[][] rotateArr = new int[K][2];
-			for (int k = 0; k < K; k++) {
+			// 자석의 회전 정보(자석의 번호, 회전방향)
+			int[][] info = new int[K][2];
+			for (int row = 0; row < K; row++) {
 				st = new StringTokenizer(br.readLine());
-				rotateArr[k][0] = Integer.parseInt(st.nextToken()); // 회전하는 자석의 번호
-				rotateArr[k][1] = Integer.parseInt(st.nextToken()); // 회전 방향
+				for (int col = 0; col < 2; col++) {
+					info[row][col] = Integer.parseInt(st.nextToken());
+				}
 			}
 
 			for (int k = 0; k < K; k++) {
-
 				visited = new boolean[5];
-
-				if (rotateArr[k][0] == 1) {
-					workMag1(rotateArr[k][1]);
-				} else if (rotateArr[k][0] == 2) {
-					workMag2(rotateArr[k][1]);
-				} else if (rotateArr[k][0] == 3) {
-					workMag3(rotateArr[k][1]);
-				} else if (rotateArr[k][0] == 4) {
-					workMag4(rotateArr[k][1]);
+				if (info[k][0] == 1) {// 1번 자석을 회전시키는 경우
+					rotateMag1(info[k][1]);
+				} else if (info[k][0] == 2) {
+					rotateMag2(info[k][1]);
+				} else if (info[k][0] == 3) {
+					rotateMag3(info[k][1]);
+				} else if (info[k][0] == 4) {
+					rotateMag4(info[k][1]);
 				}
 			}
 
 			int sum = 0;
-			sum += mag1.get(0) == 0 ? 0 : 1;
-			sum += mag2.get(0) == 0 ? 0 : 2;
-			sum += mag3.get(0) == 0 ? 0 : 4;
-			sum += mag4.get(0) == 0 ? 0 : 8;
+			sum += (mag1.get(0) == 0) ? 0 : 1;
+			sum += (mag2.get(0) == 0) ? 0 : 2;
+			sum += (mag3.get(0) == 0) ? 0 : 4;
+			sum += (mag4.get(0) == 0) ? 0 : 8;
 
-			sb.append("#" + tc + " " + sum + "\n");
+			sb.append("#").append(t).append(" ").append(sum).append("\n");
 		}
 		System.out.print(sb);
 	}
 
-	private static void workMag1(int dir) {
+	private static void rotateMag1(int dir) {
 		visited[1] = true;
 		if (mag1.get(2) != mag2.get(6) && !visited[2]) {
-			workMag2(dir * -1);
+			rotateMag2(dir * -1);
 		}
-		rotateMag(mag1, dir);
+		rotate(mag1, dir);
 	}
 
-	private static void workMag2(int dir) {
+	private static void rotateMag2(int dir) {
 		visited[2] = true;
 		if (mag1.get(2) != mag2.get(6) && !visited[1]) {
-			workMag1(dir * -1);
+			rotateMag1(dir * -1);
 		}
 		if (mag2.get(2) != mag3.get(6) && !visited[3]) {
-			workMag3(dir * -1);
+			rotateMag3(dir * -1);
 		}
-		rotateMag(mag2, dir);
+		rotate(mag2, dir);
+
 	}
 
-	private static void workMag3(int dir) {
+	private static void rotateMag3(int dir) {
 		visited[3] = true;
 		if (mag2.get(2) != mag3.get(6) && !visited[2]) {
-			workMag2(dir * -1);
+			rotateMag2(dir * -1);
 		}
 		if (mag3.get(2) != mag4.get(6) && !visited[4]) {
-			workMag4(dir * -1);
+			rotateMag4(dir * -1);
 		}
-		rotateMag(mag3, dir);
+		rotate(mag3, dir);
 
 	}
 
-	private static void workMag4(int dir) {
+	private static void rotateMag4(int dir) {
 		visited[4] = true;
 		if (mag3.get(2) != mag4.get(6) && !visited[3]) {
-			workMag3(dir * -1);
+			rotateMag3(dir * -1);
 		}
-		rotateMag(mag4, dir);
+		rotate(mag4, dir);
 
 	}
 
-	private static void rotateMag(List<Integer> mag, int dir) {
-
+	// 회전 메서드
+	private static void rotate(List<Integer> mag, int dir) {
+		// 시계방향 회전
 		if (dir == 1) {
-			int lastIdx = mag.size() - 1;
-			int lastElement = mag.get(lastIdx);
+			int lastElement = mag.get(mag.size() - 1);
 			mag.add(0, lastElement);
 			mag.remove(mag.size() - 1);
 		}
-
-		if (dir == -1) {
+		// 반시계방향 회전
+		else {
 			mag.add(mag.get(0));
 			mag.remove(0);
 		}
+
 	}
 
 }
