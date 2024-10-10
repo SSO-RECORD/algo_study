53e1910bedcca4083712d4a570e3db8f787491d3
SSO-RECORD <108278816+SSO-RECORD@users.noreply.github.com>
2024-10-10 20:41:10 +0900

    [Silver I] Title: 그림, Time: 260 ms, Memory: 55760 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -10,7 +9,6 @@ public class Main {
 	static boolean[][] visited;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
-	static int x;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,56 +27,30 @@ public class Main {
 			}
 		}
 
-		int picCnt = 0;
+		int picCnt = 0; // 그림의 개수를 저장할 변수
 		int maxCnt = Integer.MIN_VALUE;
 		for (int row = 0; row < n; row++) {
 			for (int col = 0; col < m; col++) {
 				if (arr[row][col] == 1 && !visited[row][col]) {
-					//cnt = 0;
-					x = 1;
+					cnt = 1; // 그림의 넓이를 저장할 변수
 					dfs(row, col);
 					picCnt++;
-					//System.out.println("cnt : " + x);
-					//System.out.println("maxCnt : " + maxCnt);
-					maxCnt = Math.max(maxCnt, x);
+					maxCnt = Math.max(maxCnt, cnt);
 				}
 			}
 		}
-		
-		if(picCnt == 0) {
+
+		if (picCnt == 0) {
 			maxCnt = 0;
 		}
 
-//		for (int row = 0; row < n; row++) {
-//			for (int col = 0; col < m; col++) {
-//				System.out.print(arr[row][col] + " ");
-//			}
-//			System.out.println();
-//		}
-	
+		System.out.println(picCnt);
+		System.out.println(maxCnt);
 
-	System.out.println(picCnt);
-	System.out.println(maxCnt);
-
-//	int maxValue = Integer.MIN_VALUE;if(picCnt==0)
-//	{
-//		maxValue = 0;
-//	}else
-//	{
-//		for (int row = 0; row < n; row++) {
-//			for (int col = 0; col < m; col++) {
-//				if (arr[row][col] > maxValue) {
-//					maxValue = arr[row][col];
-//				}
-//			}
-//		}
-//	}
-
-//	System.out.println(maxValue);
 	}
 
 	private static void dfs(int r, int c) {
-		
+
 		visited[r][c] = true;
 
 		for (int d = 0; d < 4; d++) {
@@ -86,8 +58,7 @@ public class Main {
 			int nc = c + dc[d];
 
 			if (isIn(nr, nc) && !visited[nr][nc] && arr[nr][nc] == 1) {
-//				cnt += 1;
-				x += 1;
+				cnt += 1;
 				dfs(nr, nc);
 			}
 		}
