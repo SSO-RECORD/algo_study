ad111582a94fa848a7d40fa7b0286c4a69a5a96c
SSO-RECORD <108278816+SSO-RECORD@users.noreply.github.com>
2024-12-12 20:58:44 +0900

    [Silver V] Title: 줄세우기, Time: 132 ms, Memory: 15212 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -8,51 +9,35 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 
-		int P = Integer.parseInt(st.nextToken()); // 테스트케이스 수
+		int P = Integer.parseInt(br.readLine().trim()); // 테스트케이스 수
 
 		for (int tc = 1; tc <= P; tc++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
 			int num = Integer.parseInt(st.nextToken()); // 테스트케이스 번호
-			int totalCnt = 0;
 
 			List<Integer> heightList = new ArrayList<>();
+			int totalCnt = 0;
 
 			for (int h = 0; h < 20; h++) {
 				int height = Integer.parseInt(st.nextToken());
-				int cnt = 0;
-				int size = heightList.size();
-				boolean check = false;
 
-				if (size == 0) {
-					heightList.add(height);
-				} else {
-					for (int i = 0; i < size; i++) {
-						if (heightList.get(i) < height) {
-							cnt++;
-							continue;
-						}
-						heightList.add(i, height);
-//						size++;
-//						i++;
-						totalCnt += size - cnt;
-						check = true;
-						break;
-
-					}
-					if (!check) {
-						heightList.add(height);
-					}
+				// 이진탐색으로 삽입 위치 찾기
+				// height값이 리스트에 존재하면 해당 인덱스 리턴, 없으면 -(삽입위치 + 1) 리턴
+				int pos = Collections.binarySearch(heightList, height);
+				if (pos < 0) {
+					pos = -(pos + 1);
 				}
 
+				// 삽입 및 이동 횟수 계산
+				heightList.add(pos, height);
+				totalCnt += heightList.size() - pos - 1;
 			}
 			sb.append(num).append(" ").append(totalCnt).append("\n");
 		}
 		System.out.print(sb);
-
 	}
 
 }
